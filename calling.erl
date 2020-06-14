2e02409d0e34a7d3b5eb98975a3f83b44a6df646
ypandya614929 <ypandya614929@gmail.com>
2020-06-14 01:02:08 -0400

    Erlang message passing project initally completed

---
+++ calling.erl
@@ -0,0 +1,32 @@
+-module(calling).
+-export([interaction/3, initiateSlaveCommunication/3]).
+-define(PROCESS_WAIT_TIME, 5000).
+
+interaction(Sender, ReceiverList, MasterID) ->
+   
+    receive
+            {Receiver, Timestamp, intro} -> 
+            	timer:sleep(rand:uniform(100)),
+				MasterID!{Sender, Receiver, Timestamp, intro},
+                Receiver!{Sender, Timestamp, reply},
+                interaction(Sender, ReceiverList, MasterID);
+
+            {Receiver, Timestamp, reply} ->  
+            	timer:sleep(rand:uniform(100)),
+				MasterID!{Sender, Receiver, Timestamp, reply},
+                interaction(Sender, ReceiverList, MasterID)
+    
+    after ?PROCESS_WAIT_TIME ->
+		io:format("~nProcess ~w has received no calls for ~w second, ending...~n",[Sender, ?PROCESS_WAIT_TIME div 1000])
+
+    end.
+
+initiateSlaveCommunication(Sender, ReceiverList, MasterID) ->
+
+ 	timer:sleep(100),
+    lists:foreach(fun(Receiver) ->
+    	{_,_, Timestamp} = erlang:timestamp(),
+    	Receiver ! {Sender, Timestamp, intro}
+		end, ReceiverList
+	),
+	interaction(Sender, ReceiverList, MasterID).
--- exchange.erl
+++ exchange.erl
@@ -1,14 +1,19 @@
 -module(exchange).
-
--export([start/0]).
+-export([start/0, initiateCommunication/0]).
+-define(MASTER_WAIT_TIME, 10000).
 
 start() ->
 	{_, Filedata} = file:consult("calls.txt"),
 	io:format("~s.~n",["*** Calls to be made ***"]),
-	startupDisplay(Filedata).
-	% io:format("~w",[Filedata]),
-	% Communication = maps:from_list(Filedata),
-	
+	startupDisplay(Filedata),
+	Communication = maps:from_list(Filedata),
+	MasterID = self(),
+	maps:fold(fun(Sender, ReceiverList, ok) ->
+		SlaveId = spawn(calling, initiateSlaveCommunication, [Sender, ReceiverList, MasterID]),
+		register(Sender, SlaveId),
+		io:format("")
+	end,ok, Communication),
+	initiateCommunication().
 
 startupDisplay([]) -> 
 	io:format("~s",["\n"]);
@@ -17,3 +22,20 @@ startupDisplay([Head|Tail]) ->
 	{Sender, ReceiverList} = Head,
 	io:format("~w: ~w~n",[Sender, ReceiverList]),
 	startupDisplay(Tail).
+
+initiateCommunication()->
+    receive
+		{Sender, Receiver, Timestamp, intro} ->
+            io:fwrite("~w received intro message from ~w [~w]",[Sender, Receiver, Timestamp]),
+            io:format("~s",["\n"]),
+            initiateCommunication();
+
+		{Sender, Receiver, Timestamp, reply} ->
+            io:fwrite("~w received reply message from ~w [~w]",[Sender, Receiver, Timestamp]),
+            io:format("~s",["\n"]),
+            initiateCommunication()
+
+    after ?MASTER_WAIT_TIME ->
+		io:format("~nMaster has received no replies for ~w seconds, ending....~n",[?MASTER_WAIT_TIME div 1000])
+    
+    end.
