952aceb55415bdb27dec8c17b177686637d7740f
ypandya614929 <ypandya614929@gmail.com>
2020-06-20 17:09:11 -0400

    project completed and ready for submission

--- calling.erl
+++ calling.erl
@@ -1,41 +1,46 @@
+% To run the program follow below commands.
+% 1) compile both the files.
+%    c(calling).
+%    c(exchange).
+% 2) run the main method of exchange file.
+%    exchange:start().
+
 -module(calling).
-%% ====================================================================
-%% API functions
-%% ====================================================================
 -export([initiateSlaveCommunication/3]).
 -define(PROCESS_WAIT_TIME, 5000).
 
+% communication between master and slave using message passing
 interaction(Sender, MasterID) ->
    
     receive
-            {Receiver, Timestamp, intro} -> 
-            	timer:sleep(rand:uniform(100)),
-            	MasterID!{Sender, Receiver, Timestamp, intro},
-            	Receiver!{Sender, Timestamp, reply},
-                interaction(Sender, MasterID);
+        {Receiver, Timestamp, intro} -> 
+        	timer:sleep(rand:uniform(100)),
+        	MasterID!{Sender, Receiver, Timestamp, intro},
+        	Receiver!{Sender, Timestamp, reply},
+            interaction(Sender, MasterID);
 
-            {Receiver, Timestamp, reply} ->  
-            	timer:sleep(rand:uniform(100)),
-				MasterID!{Sender, Receiver, Timestamp, reply},
-                interaction(Sender, MasterID)
+        {Receiver, Timestamp, reply} ->  
+        	timer:sleep(rand:uniform(100)),
+			MasterID!{Sender, Receiver, Timestamp, reply},
+            interaction(Sender, MasterID)
         
     after ?PROCESS_WAIT_TIME ->
             goodByeSlave(Sender)
 
     end.
 
-
+% generate timestamp and send the intro message
 slaveCommunication(Sender, Receiver) -> 
     {_,_,Timestamp} = erlang:timestamp(),
     Receiver ! {Sender, Timestamp, intro}.
 
-
+% send message to each slave (receivers) from the receiverlist
 initiateSlaveCommunication(Sender, ReceiverList, MasterID) ->
 	
     timer:sleep(rand:uniform(100)),
     [slaveCommunication(Sender, Receiver) || Receiver <- ReceiverList],
 	interaction(Sender, MasterID).
 
-
+% print goodbye message on slave side
 goodByeSlave(Sender) ->
     io:format("~nProcess ~w has received no calls for ~w second, ending...~n",[Sender, ?PROCESS_WAIT_TIME div 1000]).
--- exchange.erl
+++ exchange.erl
@@ -1,10 +1,15 @@
+% To run the program follow below commands.
+% 1) compile both the files.
+%    c(calling).
+%    c(exchange).
+% 2) run the main method of exchange file.
+%    exchange:start().
+
 -module(exchange).
-%% ====================================================================
-%% API functions
-%% ====================================================================
 -export([start/0]).
 -define(MASTER_WAIT_TIME, 10000).
 
+% main method to start the program
 start() ->
 	{_, Filedata} = file:consult("calls.txt"),
 	io:format("~s.~n",["** Calls to be made **"]),
@@ -14,6 +19,7 @@ start() ->
 
 startupDisplay([]) -> io:format("~s",["\n"]);
 
+% used to display the calling list
 startupDisplay([Head|Tail]) ->
 	{Sender, ReceiverList} = Head,
 	io:format("~w: ~w~n",[Sender, ReceiverList]),
@@ -21,13 +27,16 @@ startupDisplay([Head|Tail]) ->
 
 slaveProcessRegistration([]) -> ok;
 
+% register process for communicating between sender and list of receives
 slaveProcessRegistration([Head|Tail]) ->
 	{Sender, ReceiverList} = Head,
 	SlaveId = spawn(calling, initiateSlaveCommunication, [Sender, ReceiverList, self()]),
 	register(Sender, SlaveId),
 	slaveProcessRegistration(Tail).
 
+% receive message and print at master side
 initiateCommunication()->
+    
     receive
     	{Sender, Receiver, Timestamp, intro} ->
             io:fwrite("~w received intro message from ~w [~w]",[Sender, Receiver, Timestamp]),
@@ -44,5 +53,6 @@ initiateCommunication()->
 			
     end.
 
+% print goodbye message on master side
 goodByeMaster() ->
 	io:format("~nMaster has received no replies for ~w seconds, ending....~n",[?MASTER_WAIT_TIME div 1000]).
